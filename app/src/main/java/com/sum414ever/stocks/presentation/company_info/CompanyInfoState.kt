44bb3e2bc304a4fe2c45d6df52e980d79d5564d7
sum414ever <copokaalekc@gmail.com>
2022-07-14 13:48:41 +0300

    Added companu info state and view model

--- app/src/main/java/com/sum414ever/stocks/presentation/company_info/CompanyInfoState.kt
+++ app/src/main/java/com/sum414ever/stocks/presentation/company_info/CompanyInfoState.kt
@@ -1,3 +1,11 @@
 package com.sum414ever.stocks.presentation.company_info
 
-data class CompanyInfoState()
+import com.sum414ever.stocks.domain.model.CompanyInfo
+import com.sum414ever.stocks.domain.model.IntradayInfo
+
+data class CompanyInfoState(
+    val stockInfos: List<IntradayInfo> = emptyList(),
+    val company: CompanyInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
